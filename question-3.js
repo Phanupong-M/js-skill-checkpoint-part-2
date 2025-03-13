da4d290e3aa43de68b26d45bccc4781b612be3ce
Phanupong-M <panupong38240@gmail.com>
2025-03-13 14:09:09 +0700

    submitteed skill checkpoint 2

--- question-3.js
+++ question-3.js
@@ -1,8 +1,12 @@
 async function getUsers(){
-    const userPost = await fetch("https://jsonplaceholder.typicode.com/users")
-    const data = await userPost.json()
-    const result = data.map(getName => getName.name).filter(checkName => checkName.length > 17)
-    console.log(result)
+    try{
+        const userPost = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await userPost.json()
+        const result = data.map(getName => getName.name).filter(checkName => checkName.length > 17)
+        console.log(result)
+    }catch(error){
+        console.log(error)
+    }
 }
 
 getUsers()
